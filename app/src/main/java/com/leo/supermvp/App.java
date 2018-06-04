6b47f1f43b768fb5ba401210560f303295ed7ffa
LeXiaoWen <LeXiaoWen_Android@163.com>
2018-06-05 00:32:19 +0800

    测试工具类是否可用

---
+++ app/src/main/java/com/leo/supermvp/App.java
@@ -0,0 +1,15 @@
+package com.leo.supermvp;
+
+import com.leo.mvp.utils.BaseApp;
+
+/**
+ * created by Leo on 2018/6/5 00 : 21
+ */
+
+
+public class App extends BaseApp {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/leo/supermvp/MainActivity.java
+++ app/src/main/java/com/leo/supermvp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.leo.supermvp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.leo.mvp.utils.log.LogUtils;
+import com.leo.mvp.utils.toast.ToastUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnTest = (Button) findViewById(R.id.btn_test);
+        btnTest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_test:
+                LogUtils.e("这是测试信息");
+                ToastUtils.showShortToast("这是测试信息");
+                break;
+        }
     }
 }
